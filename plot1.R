dea608d10cc75a577f3d30a30ce40f7df45f1654
Amer Malik <ameryusuf@yahoo.com>
2015-07-12 20:05:55 +0500

    Script for the first plot
    Edited the script for the first plot which resulted in generating the
    required plot1.png file

--- plot1.R
+++ plot1.R
@@ -1,12 +1,34 @@
 
 ## Download the zip file from the url
-url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(url, destfile = "./data/Electric Power Consumption.zip", method = "internal")
 
-## Unzip the contents of the file to the working directory
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("./data/Electric Power Consumption.zip")){
+
+download.file(url, destfile = "./data/Electric Power Consumption.zip", method = "internal")
 unzip("./data/Electric Power Consumption.zip", exdir = "./data")
 
-## Load the dataset into R using the read.table function
-powercon<-read.table("./data/household_power_consumption.txt", sep =";", header = TRUE)
+}
 
-##
+
+## Load the dataset into R using the read.table function
+powercon<-read.table("./data/household_power_consumption.txt", sep =";", header = TRUE, na.strings = "?")
+
+## Convert Date and Time to Date & Time format
+powercon$Date<-as.Date(as.character(powercon$Date), "%m/%d/%Y")
+powercon$Time<-strptime(powercon$Time, "%H:%M:%S")
+
+
+##Subset the dataset for the two February dates
+date1<-as.Date("2007-02-01")
+date2<-as.Date("2007-02-02")
+plotdata<-subset(powercon, Date >= date1 & Date <= date2)
+
+## Plot the graph to plot1.png file
+x<-plotdata$Global_active_power
+png("./data/ExData_Plotting1/plot1.png", width = 480, height = 480)
+
+hist(x, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylim = c(0,1200), xlim = c(0,6), xaxp = c(0,6,3))
+
+dev.off()
+
